21e4915285665e0a7d6f68d34989bfc8ba67e269
vignesh chandran balan <vigneshcbv@yahoo.co.in>
2021-04-22 01:04:10 -0500

    Computer Vision project

---
+++ agile-computervision/DriverScript.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace agile_computervision
+{
+    class CropDiseaseDetection
+    {
+        static void Main(string[] args)
+        {
+            /*Get All Image from Input Folder*/
+            var files = FileHelpers.GetImages();
+            Console.WriteLine(files.ToString());
+
+        }
+
+    }
+}
---
+++ agile-computervision/FileHelpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace agile_computervision
+{
+    public class FileHelpers
+    {
+        public static dynamic GetImages()
+        {
+            return GetFiles("Input");
+        }
+
+        /*To Filter .jpg & .jpeg Images*/
+        private static Func<string, bool> FilterImage = (filename) => { return Regex.IsMatch(filename, @".jpeg|.jpg$"); };
+
+        private static Func<string, List<string>> GetFiles = (folderName) =>
+        {
+            string workingDirectory = Environment.CurrentDirectory;
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
+            return Directory.GetFiles($"{projectDirectory}/{folderName}", "*.*", SearchOption.AllDirectories).Where(f => FilterImage(f))
+                            .Select(f => f)
+                            .ToList();
+        };
+    }
+}
